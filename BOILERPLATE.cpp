e32c0aa55ae57eff26aa416cc1d03fadf7390f09
Hrithik Joshi <40289232+hrithik01@users.noreply.github.com>
2024-03-07 03:36:26 +0530

    Boilerplate file to quick start with

---
+++ BOILERPLATE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main () {
+    cout<<"Hello From Hrithik \n";
+    return 0;
+}
+
+// g++ filetorun.cpp -o outputfilename      - To compile the file.
+// ./outputfilename      - To run the file.
+// g++ -std=c++11 filetorun.cpp -o outputfilename    - To run specific version of c++.
