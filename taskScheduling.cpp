2622764a9e2a668a09bd0a4948fe79961028de95
Hrithik Joshi <40289232+hrithik01@users.noreply.github.com>
2024-05-02 11:05:01 +0530

    Refactor task scheduling code to improve efficiency

--- taskScheduling.cpp
+++ taskScheduling.cpp
@@ -35,6 +35,28 @@ int LeastTimeToComplete(const std::vector<char>& tasks, int n) {
     return all_time + idle_time;
 }
 
+int LeastTimeToComplete2(vector<char>& tasks, int n) {
+    unordered_map<char, int> mp;
+    for(char c: tasks)
+        mp[c]++;
+    vector<pair<char,int>> v(mp.begin(), mp.end());
+    sort(v.begin(), v.end(), [] (pair<char, int> p1, pair<char, int> p2) {
+        return p1.second < p2.second;
+    });
+    int max_freq = v.back().second - 1;
+    v.pop_back();
+
+    int idle_time = (max_freq - 1)*n;
+
+    while(!v.empty() && idle_time > 0) {
+        idle_time -= min(max_freq - 1, v.back().second);
+        v.pop_back();
+    }
+
+    idle_time = max(0, idle_time);
+    return tasks.size() + idle_time;
+}
+
 int main() {
     vector<char> tasks = {'A', 'A', 'B', 'B'};
     int n = 2;
