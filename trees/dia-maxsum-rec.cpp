78c62722b2bd1de12cd27fb3a5d232ec9ae759c1
Hrithik Joshi <40289232+hrithik01@users.noreply.github.com>
2024-12-05 16:07:31 +0530

    width of binary tree

--- trees/dia-maxsum-rec.cpp
+++ trees/dia-maxsum-rec.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -46,6 +47,24 @@ bool balancedTree(tnode* root) {
 bool isSameTree(tnode* r1, tnode* r2) {
     if(!r1 || !r2) return r1==r2;
     return r1->data == r2->data && isSameTree(r1->left,r2->left) && isSameTree(r1->right,r2->right);
+}
+
+int width_tress(tnode* root) {
+    if(!root) return 0;
+    int max_width = 0;
+    queue<tnode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        int size = q.size();
+        max_width = max(max_width, size);
+        for(int i=0; i<size; i++) {
+            tnode* temp = q.front();
+            q.pop();
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+    }
+    return max_width;  
 }
 
 int main() {
