aff29e3d105defb858b1ad9b7f9151f3c3612f50
Hrithik Joshi <40289232+hrithik01@users.noreply.github.com>
2025-04-26 15:16:17 +0530

    feat: Add optimized function for longest common prefix and improve existing implementation

--- arrays-strings/strings/strPrefixMatch.cpp
+++ arrays-strings/strings/strPrefixMatch.cpp
@@ -24,6 +24,41 @@ bool isSuffix(string i, string s) {
     return true;
 }
 
+// O(n*m)
+// O(n*m) - where n is the number of strings and m is the length of the longest string
+// O(1) - space complexity
+string longestCommonPrefixOptimised(vector<string> vec) {
+    if (vec.empty()) return "";
+    string prefix = vec[0];
+    int n = vec.size();
+    int m = prefix.size();
+    for (int i = 0; i < m; ++i) {
+        char c = prefix[i];
+        for (int j = 1; j < n; ++j) {
+            if (i == vec[j].size() || vec[j][i] != c)
+                return prefix.substr(0, i);
+        }
+    }
+    return prefix;
+}
+
+// O(n*m)
+// O(1) - space complexity
+string longestCommonPrefix(vector<string>& vec) {
+    if (vec.empty()) return "";
+    string ans = vec[0];
+    for(int i=1; i<vec.size(); i++) {
+        int j = min(ans.size(), vec[i].size());
+        ans = ans.substr(0, j);
+        while(j>0 && vec[i].substr(0,j) != ans.substr(0, j)) {
+            j--;
+        }
+        ans = ans.substr(0, j);
+        if(j==0) return "";
+    }
+    return ans;
+}
+
 int main() {
     vector<string> arr = {"abab", "aba", "abc", "aaa", "abaa", "abba", "baaa", "bbaa"};
     string s = "abaabbabbaa";
